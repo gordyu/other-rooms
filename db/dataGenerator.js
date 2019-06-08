74ab71152702ecd54d7b3699411ff28fdd72c681
jvc5546 <jvc5546@gmail.com>
2019-06-08 10:55:47 -0500

    Writting generated data into data.txt file

--- db/dataGenerator.js
+++ db/dataGenerator.js
@@ -1,6 +1,9 @@
 const faker = require('faker');
+const fs = require('fs');
+const path = require('path');
 
-const seedData = [];
+// const seedData = [];
+const filepath = path.join(__dirname, '/data.txt');
 
 generate = (id) => {
   let entry = {};
@@ -26,11 +29,41 @@ generate = (id) => {
   return entry;
 };
 
-for(let i = 0; i < 100000; i++) {//Breaks when using 1,000,000 Documents
-  let newHome = generate(i);
-  seedData.push(newHome);
-}
+// for(let i = 0; i < 100000; i++) {//Breaks when using 1,000,000 Documents
+//   let newHome = generate(i);
+//   seedData.push(newHome);
 
-console.log(`${seedData.length} Documents generated.`);
-
-module.exports.data = seedData;
+// }
+console.log(`Creating data.txt file at ${Date()}`);
+fs.writeFile(filepath, '', (err) => {
+  if (err){
+    console.log(`Error creating data.txt file: ${err}`);
+    return;
+  }
+  console.log(`Successfully created data.txt at ${Date()}`);
+  console.log(`Opening data.txt file at ${Date()}`);
+  fs.open(filepath,'r+', (err, fd) => {
+    if(err){
+      console.log(`Error opening document`);
+      return;
+    }
+    let limit = 10000000;
+    console.log(`Successfully opened data.txt file at ${Date()}`);
+    console.log(`Creating ${limit} rows for data.txt file at ${Date()}`);
+    let writeHome = function(count) {
+      if(count >= limit){
+        console.log(`Successfully generated ${count} rows at ${Date()}`);
+        return;
+      }
+      let {id, type, tags, price, description, location, rating, numRatings, image} = generate(count);
+      fs.write(fd, `${id}\t${type}\t ${tags}\t ${price}\t ${description}\t ${location}\t ${rating}\t ${numRatings}\t${image}\n`, (err) => {
+        if(err){
+          console.log(`Error writting row ${count}: ${err}`);
+          return;
+        }
+        writeHome(count + 1);
+      });
+    }
+    writeHome(0);
+  });
+});
